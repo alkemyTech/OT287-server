a6b41996c5e62e55bd662a76860a5e5b6bd45782
GregoHouse <96206226+GregoHouse@users.noreply.github.com>
2022-09-16 12:13:33 -0300

    [SLIDES] ot287-31-create migration & model Slide (#11)
    * [SLIDES] ot287-31-create migration & model Slide
    
    * OT287-31-fix organizationId name column
    
    * OT287-31-fix Text:DataType.Text & fix associate model
    
    * OT287-31-fix migration text type & model gramatical error
    
    * OT287-31-fix migration remove onDelete,onUpdate,deletedAt
    
    Co-authored-by: “GregoryHouse” <“gregorioromerogarzon@gmail.com”>

---
+++ database/models/slide.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Slide extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Slide.belongsTo(models.Organization, {as: 'organizationId'});
+    }
+  };
+  Slide.init({
+    imageUrl: DataTypes.STRING,
+    text: DataTypes.TEXT,
+    order: DataTypes.INTEGER,
+    organizationId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Slide',
+  });
+  return Slide;
+};
