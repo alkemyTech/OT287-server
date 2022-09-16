c5df56d3a1c2b72439c5ed2a8c26fbb0f818e2bd
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-16 14:08:10 -0300

    Added news controller

---
+++ controllers/entry.js
@@ -0,0 +1,23 @@
+const createHttpError = require('http-errors')
+const { getNews } = require('../services/entry')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
+
+module.exports = {
+  get: catchAsync(async (req, res, next) => {
+    try {
+      const response = await getNews()
+      endpointResponse({
+        res,
+        message: 'News retrieved successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving news] - [news - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+}
