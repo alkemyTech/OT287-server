833f883ed78c03f739879b1fd5acc1c46d6261c8
Eber <85371377+ebereber@users.noreply.github.com>
2022-09-16 15:02:53 -0300

    [CATEGORIES] Create model and migration for Categories OT287-42 (#7)
    * Create model and migration for Categories
    
    * Create model and migration for Categories
    
    * update migration file for Categories

---
+++ database/migrations/20220915122858-create-category.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories');
+  }
+};
