2b96e600d60d7caa0176b5c530861c95375bacf6
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-16 19:14:22 -0300

    fixed router position

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cors = require('cors')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index')
-const entryRouter = require('./routes/entry')
 
 const port = process.env.PORT || 3000
 
@@ -21,7 +20,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/news', entryRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
