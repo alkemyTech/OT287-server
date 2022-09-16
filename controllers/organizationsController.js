eec2fbfc8574cdaff1f2a568d0f4b75747629871
Guiniote <guiniote@gmail.com>
2022-09-16 11:43:05 -0300

    Create controller and service to search in db the organization info

---
+++ controllers/organizationsController.js
@@ -0,0 +1,24 @@
+const createHttpError = require('http-errors')
+const { getInfo } = require('../services/organizationsService')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
+
+// Method to get an organization public info
+module.exports = {
+  get: catchAsync(async (req, res, next) => {
+    try {
+      const response = await getInfo(req.params.id)
+      endpointResponse({
+        res,
+        message: 'Info retrieved successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving info] - [index - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+}
