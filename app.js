ea0bb7999ddec7d012d26e1535f90cdf866ec69e
Guiniote <guiniote@gmail.com>
2022-09-16 11:41:31 -0300

    Create router for the organization info and endpoint for it's public info

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index')
+const organizationsRouter = require('./routes/organizationsRouter')
 
 const port = process.env.PORT || 3000
 
@@ -20,6 +21,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+app.use('/organizations', organizationsRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
