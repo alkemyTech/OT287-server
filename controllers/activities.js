223dc65168e21b926ff15abb6f2c8122aa72366d
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-17 16:55:56 -0300

    Added activity update controller

---
+++ controllers/activities.js
@@ -0,0 +1,25 @@
+const createHttpError = require('http-errors')
+const { putActivity } = require('../services/activities')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
+
+module.exports = {
+  put: catchAsync(async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const dataToUpdate = req.body
+      const response = await putActivity(id, dataToUpdate)
+      endpointResponse({
+        res,
+        message: 'Activity updated and retrieved successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error updating activity] - [activity - PUT]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+}
