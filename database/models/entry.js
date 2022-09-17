a90fd657e81aa5fc800d9089843e581f01d86270
Gabriel <pein4302@gmail.com>
2022-09-17 18:27:37 -0300

    OT287-41-fix

--- database/models/entry.js
+++ database/models/entry.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'Entry',
     paranoid: true,
-    timestamps: false,
+    timestamps: true,
   });
   return Entry;
 };
