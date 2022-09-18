10a828dbb35711dcc9122aadbaa5cb7617f4cb3d
MrLuis-WebMaster <mr.luiswebmaster@gmail.com>
2022-09-18 15:53:28 -0500

    rename route for testimonials

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
-router.use('/testimonial', createTestimonial)
+router.use('/testimonials', createTestimonial)
 router.use('/activities', activitiesRouter)
 router.use('/news', entriesRouter)
 
