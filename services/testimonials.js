7c54de0a52f752f90819b296de3871ada3cf5424
MrLuis-WebMaster <mr.luiswebmaster@gmail.com>
2022-09-18 15:52:37 -0500

    removed validation in services

--- services/testimonials.js
+++ services/testimonials.js
@@ -3,18 +3,13 @@ const { Testimonial } = require('../database/models')
 
 exports.createTestimonial = async (name, content, image) => {
     try {
-        if (!name || !content || !image) {
-            throw new ErrorObject('No content!', 204)
-        }
         const testimonial = await Testimonial.create({
             name:name,
             content:content,
             image:image
         })
         if (!testimonial) throw new ErrorObject('Error!', 400)
-        
         return testimonial
-        
     } catch (error) {
         throw new ErrorObject(error.message, error.statusCode || 500)
     }
