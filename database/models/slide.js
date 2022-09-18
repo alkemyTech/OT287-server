5f4d2b96aa18dff1b509ef175e30d8dbb96803af
eli-beg <begaeliana@gmail.com>
2022-09-18 16:53:34 -0300

    Fix grammar error in Slide association

--- database/models/slide.js
+++ database/models/slide.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Slide.belongsTo(models.Organization, {as: 'organization'});
+      Slide.belongsTo(models.Organization, {as: 'organizations'});
     }
   };
   Slide.init({
