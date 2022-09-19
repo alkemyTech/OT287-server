76caac8eece2d770d660c8fc219cc7501e456cc9
Guiniote <guiniote@gmail.com>
2022-09-19 01:20:12 -0300

    Create route, controller method and service function

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,5 +1,5 @@
 const createHttpError = require('http-errors')
-const { editActivity } = require('../services/activities')
+const { editActivity, createActivity } = require('../services/activities')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
@@ -16,6 +16,25 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error updating activity] - [activity - PUT]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+
+  post: catchAsync(async (req, res, next) => {
+    try {
+      await createActivity({
+        ...req.body,
+      })
+      endpointResponse({
+        res,
+        code: 201,
+        message: 'Activity created successfully',
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error creating activity] - [index - POST]: ${error.message}`,
       )
       next(httpError)
     }
--- routes/activities.js
+++ routes/activities.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { put } = require('../controllers/activities')
+const { put, post } = require('../controllers/activities')
 
 const router = express.Router()
 
 router.put('/:id', put)
+router.post('/', post)
 
 module.exports = router
