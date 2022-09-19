0f646fa628fa9b9ce9d67b8e5e45e2a72ac8af14
Gabriel <pein4302@gmail.com>
2022-09-19 18:40:46 -0300

    deleted validation.js to avoid conflict with main branch

--- routes/entries.js
+++ routes/entries.js
@@ -2,11 +2,11 @@ const express = require('express')
 const { get } = require('../controllers/index')
 const { post } = require('../controllers/entries')
 const { validationResultsSchema } = require('../schemas/entries')
-const { validateEntry } = require('../middlewares/validations')
+const { validate } = require('../middlewares/validations')
 
 const router = express.Router()
 
-router.post('/', validateEntry(validationResultsSchema), post)
+router.post('/', validate(validationResultsSchema), post)
 
 router.get('/', get)
 
