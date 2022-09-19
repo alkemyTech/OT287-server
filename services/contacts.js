7a58143e3651431507e8eddd289bcd07c160c108
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-19 17:21:52 -0300

    removed unnecessary conditional

--- services/contacts.js
+++ services/contacts.js
@@ -4,9 +4,6 @@ const { Contact } = require('../database/models')
 exports.createContact = async (data) => {
   try {
     const contact = Contact.create({ ...data })
-    if (!contact) {
-      throw new Error('Your client has issued a malformed or illegal request', 400)
-    }
     return contact
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
