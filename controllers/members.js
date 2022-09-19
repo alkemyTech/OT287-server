874c39cbd735344f24ceee85cb4165f3ad478724
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-19 19:38:18 -0300

    Added delete member controller

--- controllers/members.js
+++ controllers/members.js
@@ -1,5 +1,5 @@
 const createHttpError = require('http-errors')
-const { getMembers } = require('../services/members')
+const { getMembers, deleteMember } = require('../services/members')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
@@ -17,6 +17,22 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error retrieving members] - [members - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+  destroy: catchAsync(async (req, res, next) => {
+    try {
+      const response = await deleteMember(req.params.id)
+      endpointResponse({
+        res,
+        message: 'Member deleted successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error deleting member] - [members - DELETE]: ${error.message}`,
       )
       next(httpError)
     }
