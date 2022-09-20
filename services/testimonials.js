f3e72e94b1fb8b53d1b5f8a62ef440b49062daa9
Guiniote <guiniote@gmail.com>
2022-09-20 00:46:26 -0300

    Create put testimonials endpoint (router, controller, service)

--- services/testimonials.js
+++ services/testimonials.js
@@ -12,3 +12,16 @@ exports.deleteById = async (testimonialId) => {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
 }
+
+exports.editTestimonial = async (id, data) => {
+  try {
+    const testimonial = await Testimonial.findByPk(id)
+    if (!testimonial) {
+      throw new ErrorObject('No testimonial found', 404)
+    }
+    const updated = await testimonial.update({ ...data })
+    return updated
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
