606b8daa9b1de9f701c00cbba4609389e596ef2a
MrLuis-WebMaster <mr.luiswebmaster@gmail.com>
2022-09-19 21:45:57 -0500

    add routers

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,11 @@ const router = express.Router()
 // example of a route with index controller get function
 router.get('/', get)
 router.use('/testimonials', testimonialsRouter)
+router.use('/organizations', organizationsRouter)
+router.use('/activities', activitiesRouter)
 router.use('/news', entriesRouter)
 router.use('/contacts', contactsRouter)
 router.use('/members', membersRouter)
+
 
 module.exports = router
