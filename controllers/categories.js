ea8269c34be8525b9397e9a6f6c1675419c31900
Gabriel <pein4302@gmail.com>
2022-09-20 13:51:28 -0300

    created endpoint to update categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,7 +1,7 @@
 const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
-const { getCategories } = require('../services/categories')
+const { getCategories, updateCategory } = require('../services/categories')
 
 module.exports = {
   get: catchAsync(async (req, res, next) => {
@@ -16,6 +16,22 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error retrieving categories] - [categories - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+  put: catchAsync(async (req, res, next) => {
+    try {
+      const response = await updateCategory(req.params.id, req.body)
+      endpointResponse({
+        res,
+        message: 'Category updated successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error updating categories] - [categories - PUT]: ${error.message}`,
       )
       next(httpError)
     }
--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { get } = require('../controllers/categories')
+const { get, put } = require('../controllers/categories')
 
 const router = express.Router()
 
 router.get('/', get)
+router.put('/:id', put)
 
 module.exports = router
