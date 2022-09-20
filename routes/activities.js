6ee3a01e56de4a8029cb52ff07789703de651b1e
German Ghio <79290983+guiniote@users.noreply.github.com>
2022-09-20 12:33:46 -0300

    [ENDPOINT] OT287-67: POST /activities (#32)
    * Create route, controller method and service function with validations

--- routes/activities.js
+++ routes/activities.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { put } = require('../controllers/activities')
+const { put, post } = require('../controllers/activities')
+const { activities } = require('../schemas/activities')
+const { validate } = require('../middlewares/validations')
 
 const router = express.Router()
 
 router.put('/:id', put)
+router.post('/', validate(activities), post)
 
 module.exports = router
