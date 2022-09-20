aad26b003be647da5fa2c8063a555654a8a0b320
Guiniote <guiniote@gmail.com>
2022-09-19 23:59:10 -0300

    Validation implemented

--- routes/activities.js
+++ routes/activities.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { put, post } = require('../controllers/activities')
+const { activities } = require('../schemas/activities')
+const { validate } = require('../middlewares/validations')
 
 const router = express.Router()
 
 router.put('/:id', put)
-router.post('/', post)
+router.post('/', validate(activities), post)
 
 module.exports = router
