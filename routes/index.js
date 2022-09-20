f26fece24adf93bf39efbb1860e0abe5b4f6e2c7
Federico Capucci <federicocapucci@hotmail.com>
2022-09-20 18:02:55 -0300

    [ENDPOINT] OT287-33: POST /auth/register   User Registration Endpoint (#17)
    * Installed bcrypt
    
    * Add auth route and update index route
    
    * Added controller, schema validator and service for user creation
    
    * edited names, moved code to correct files as per project structure
    
    * Updated files with requested name changes on variables
    
    * add schema formatted and edited router to use middleware
    
    * Schema rework and translation
    
    * Adding missing translation
    
    * moving brcrypt logic into service,editing controller logic
    
    * refactored bcrypt code
    
    * refactored users service

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { get } = require('../controllers/index')
+const authRouter = require('./users')
 const organizationsRouter = require('./organizations')
 const activitiesRouter = require('./activities')
 const entriesRouter = require('./entries')
@@ -12,6 +13,7 @@ const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
+router.use('/auth', authRouter)
 router.use('/organizations', organizationsRouter)
 router.use('/activities', activitiesRouter)
 router.use('/news', entriesRouter)
