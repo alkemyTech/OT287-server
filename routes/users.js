0f9bd6ec931345687b39d146aefeb1a520fe44c2
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-22 15:40:21 -0300

    added login route

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
 const router = express.Router()
-const { register } = require('../schemas/users')
-const { post, destroy } = require('../controllers/users')
+const { register, loginSchema } = require('../schemas/users')
+const { post, destroy, login } = require('../controllers/users')
 const { validate } = require('../middlewares/validations')
 
 router.post('/register', validate(register), post)
+router.post('/login', validate(loginSchema), login)
 router.delete('/:id', destroy)
 module.exports = router
