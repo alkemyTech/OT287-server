a8093a9ee65ff09e0b66c9fb1708129897a05737
Guiniote <guiniote@gmail.com>
2022-09-23 16:54:50 -0300

    Fix error handling in users controller and service

--- controllers/users.js
+++ controllers/users.js
@@ -8,15 +8,11 @@ module.exports = {
   post: catchAsync(async (req, res, next) => {
     try {
       const response = await createUser(req.body)
-      if (!response) {
-        res.send({ code: 409, msg: 'Email already in use, try another email address' })
-      } else {
-        endpointResponse({
-          res,
-          message: 'User registered successfully',
-          body: response,
-        })
-      }
+      endpointResponse({
+        res,
+        message: 'User registered successfully',
+        body: response,
+      })
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
