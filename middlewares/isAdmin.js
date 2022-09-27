a8cbbafe5facb7b8a6c15801815ece407b3c2a14
“GregoryHouse” <gregorioromerogarzon@gmail.com>
2022-09-27 17:33:00 -0300

    OT287-53 fix

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,22 +1,19 @@
 const { ErrorObject } = require('../helpers/error')
 const { decodeToken } = require('../middlewares/JWT')
 
-const isAdmin = (allowedRoleId) => (req, res, next) => {
+exports.isAdmin = (req, res, next) => {
   try {
     let token = req.headers.authorization
     if (!token) {
       throw new ErrorObject('Unauthorized', 401)
     }
     const userRoleId = decodeToken(token).roleId
-   
-    if(allowedRoleId.includes(userRoleId)){
+    if(userRoleId === 1){
       return next()
-    } else {
-      throw new ErrorObject('Credentials not allowed', 404)
-    }
+    } 
+    throw new ErrorObject('Credentials not allowed', 404)
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
 }
 
-module.exports = isAdmin
