3e12839e685367fc3b1f24979582241b09559ab6
Eber Leguizamon <eberlegza@gmail.com>
2022-09-29 23:44:33 -0300

    feat: add put endpoint to users route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { destroy, get } = require('../controllers/users')
+const { destroy, get, put } = require('../controllers/users')
 const { verifyToken } = require('../middlewares/JWT')
 const isAdmin = require('../middlewares/isAdmin')
 
@@ -7,5 +7,6 @@ const router = express.Router()
 
 router.delete('/:id', destroy)
 router.get('/', verifyToken, isAdmin(), get)
+router.put('/:id', put)
 
 module.exports = router
