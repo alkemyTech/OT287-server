055a74c564eac7697d7370c0e6f83baa0d9ab41f
GregoHouse <96206226+GregoHouse@users.noreply.github.com>
2022-10-02 12:00:33 -0300

    OT287-62 endpoint getActivityById for Activity Edit Form (client) (#54)
    Co-authored-by: “GregoryHouse” <“gregorioromerogarzon@gmail.com”>

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,9 +1,25 @@
 const createHttpError = require('http-errors')
-const { editActivity, createActivity } = require('../services/activities')
+const { editActivity, createActivity, getActivityById } = require('../services/activities')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
 module.exports = {
+  getById: catchAsync(async (req, res, next) => {
+    try {
+      const response = await getActivityById(req.params.id)
+      endpointResponse({
+        res,
+        message: 'Activity retrieved successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving activity] - [activity - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
   put: catchAsync(async (req, res, next) => {
     try {
       const response = await editActivity(req.params.id, req.body)
--- services/activities.js
+++ services/activities.js
@@ -1,6 +1,18 @@
 const { ErrorObject } = require('../helpers/error')
 const { Activity } = require('../database/models')
 
+exports.getActivityById = async (idNew) => {
+  try {
+    const getActivity = await Activity.findByPk(idNew)
+    if (!getActivity) {
+      throw new ErrorObject('No activity found', 404)
+    }
+    return getActivity
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
+
 exports.editActivity = async (id, data) => {
   try {
     const activity = await Activity.findByPk(id)
