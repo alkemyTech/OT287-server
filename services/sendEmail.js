d5960bd6a901d8a6c0d1de6187367360a217ab2e
Gabriel <pein4302@gmail.com>
2022-10-03 19:58:25 -0300

    functional version

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -1,14 +1,14 @@
 const sgMail = require('@sendgrid/mail')
 const { ErrorObject } = require('../helpers/error')
 
-exports.sendEmail = async (email) => {
+exports.sendEmail = async (email, nombre) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
     to: email,
-    from: 'test@gmail.com', // Change to your verified sender
-    subject: 'Subject Test',
+    from: process.env.SENDGRID_VERIFIED_EMAIL, // Change to your verified sender
+    subject: 'Bienvenido a ONG Alkemy!',
     text: 'Text Test',
-    html: '<strong>Send grid config test</strong>',
+    html: `<strong>Bienvenido ${nombre}! Gracias por registrarte en nuestra ONG Alkemy, tus datos han sido validados correctamente, ya puedes logearte!</strong>`,
   }
   try {
     await sgMail.send(msg)
