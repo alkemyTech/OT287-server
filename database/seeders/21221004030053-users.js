53e9872304509ede10362875e5f684b485654654
Corxario <federicocapucci@hotmail.com>
2022-10-17 19:41:38 -0300

    Make id dynamic in users seeder, delete demo user and add it to users seeder

--- database/seeders/21221004030053-users.js
+++ database/seeders/21221004030053-users.js
@@ -5,8 +5,16 @@ const bcrypt = require('bcrypt')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const encryptedPassword = await bcrypt.hash("Init123!", 10)
-    await queryInterface.bulkInsert('Users', [{
-      "id": 1,
+    await queryInterface.bulkInsert('Users', [
+      {      
+        firstName: 'Usuario',
+        lastName: 'Demo',
+        email: 'test@test.com',
+        password: encryptedPassword,
+        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        roleId: 1,        
+      },
+      {      
       "firstName": "Gottfried",
       "lastName": "Bangle",
       "email": "gbangle0@canalblog.com",
@@ -14,7 +22,6 @@ module.exports = {
       "image": "http://dummyimage.com/112x100.png/5fa2dd/ffffff",
       "roleId": 1,
     }, {
-      "id": 2,
       "firstName": "Chelsy",
       "lastName": "Bezarra",
       "email": "cbezarra1@artisteer.com",
@@ -22,7 +29,6 @@ module.exports = {
       "image": "http://dummyimage.com/187x100.png/cc0000/ffffff",
       "roleId": 1,
     }, {
-      "id": 3,
       "firstName": "Klarika",
       "lastName": "Dorey",
       "email": "kdorey2@kickstarter.com",
@@ -30,7 +36,6 @@ module.exports = {
       "image": "http://dummyimage.com/192x100.png/dddddd/000000",
       "roleId": 1,
     }, {
-      "id": 4,
       "firstName": "Nelle",
       "lastName": "Eagling",
       "email": "neagling3@nifty.com",
@@ -38,7 +43,6 @@ module.exports = {
       "image": "http://dummyimage.com/204x100.png/5fa2dd/ffffff",
       "roleId": 1,
     }, {
-      "id": 5,
       "firstName": "Blayne",
       "lastName": "Greenrde",
       "email": "bgreenrde4@upenn.edu",
@@ -46,7 +50,6 @@ module.exports = {
       "image": "http://dummyimage.com/139x100.png/ff4444/ffffff",
       "roleId": 1,
     }, {
-      "id": 6,
       "firstName": "Karie",
       "lastName": "Bramsen",
       "email": "kbramsen5@ifeng.com",
@@ -54,7 +57,6 @@ module.exports = {
       "image": "http://dummyimage.com/155x100.png/cc0000/ffffff",
       "roleId": 1,
     }, {
-      "id": 7,
       "firstName": "Vyky",
       "lastName": "Bailes",
       "email": "vbailes6@mapquest.com",
@@ -62,7 +64,6 @@ module.exports = {
       "image": "http://dummyimage.com/143x100.png/cc0000/ffffff",
       "roleId": 1,
     }, {
-      "id": 8,
       "firstName": "James",
       "lastName": "Clemonts",
       "email": "jclemonts7@goo.ne.jp",
@@ -70,7 +71,6 @@ module.exports = {
       "image": "http://dummyimage.com/201x100.png/5fa2dd/ffffff",
       "roleId": 1,
     }, {
-      "id": 9,
       "firstName": "Franky",
       "lastName": "Tingey",
       "email": "ftingey8@apple.com",
@@ -78,7 +78,6 @@ module.exports = {
       "image": "http://dummyimage.com/184x100.png/dddddd/000000",
       "roleId": 1,
     }, {
-      "id": 10,
       "firstName": "Violetta",
       "lastName": "Creighton",
       "email": "vcreighton9@geocities.jp",
@@ -86,7 +85,6 @@ module.exports = {
       "image": "http://dummyimage.com/214x100.png/cc0000/ffffff",
       "roleId": 1,
     }, {
-      "id": 11,
       "firstName": "Ichabod",
       "lastName": "Managh",
       "email": "imanagh0@google.nl",
@@ -94,7 +92,6 @@ module.exports = {
       "image": "http://dummyimage.com/171x100.png/dddddd/000000",
       "roleId": 2,
     }, {
-      "id": 12,
       "firstName": "Jobye",
       "lastName": "Banaszczyk",
       "email": "jbanaszczyk1@ehow.com",
@@ -102,7 +99,6 @@ module.exports = {
       "image": "http://dummyimage.com/226x100.png/5fa2dd/ffffff",
       "roleId": 2,
     }, {
-      "id": 13,
       "firstName": "Zebedee",
       "lastName": "McDonnell",
       "email": "zmcdonnell2@whitehouse.gov",
@@ -110,55 +106,48 @@ module.exports = {
       "image": "http://dummyimage.com/101x100.png/dddddd/000000",
       "roleId": 2,
     }, {
-      "id": 14,
       "firstName": "Zack",
       "lastName": "Elcott",
       "email": "zelcott3@marriott.com",
       "password": encryptedPassword,
       "image": "http://dummyimage.com/226x100.png/dddddd/000000",
       "roleId": 2,
-    }, {
-      "id": 15,
+    }, {  
       "firstName": "Nico",
       "lastName": "Renachowski",
       "email": "nrenachowski4@bloglovin.com",
       "password": encryptedPassword,
       "image": "http://dummyimage.com/217x100.png/dddddd/000000",
       "roleId": 2,
-    }, {
-      "id": 16,
+    }, {  
       "firstName": "Biddy",
       "lastName": "Etherton",
       "email": "betherton5@yandex.ru",
       "password": encryptedPassword,
       "image": "http://dummyimage.com/162x100.png/cc0000/ffffff",
       "roleId": 2,
-    }, {
-      "id": 17,
+    }, {  
       "firstName": "Lutero",
       "lastName": "Brokenshire",
       "email": "lbrokenshire6@wisc.edu",
       "password": encryptedPassword,
       "image": "http://dummyimage.com/148x100.png/5fa2dd/ffffff",
       "roleId": 2,
-    }, {
-      "id": 18,
+    }, {  
       "firstName": "Franny",
       "lastName": "Samwayes",
       "email": "fsamwayes7@typepad.com",
       "password": encryptedPassword,
       "image": "http://dummyimage.com/100x100.png/cc0000/ffffff",
       "roleId": 2,
-    }, {
-      "id": 19,
+    }, {  
       "firstName": "Katrinka",
       "lastName": "Kreuzer",
       "email": "kkreuzer8@opera.com",
       "password": encryptedPassword,
       "image": "http://dummyimage.com/141x100.png/cc0000/ffffff",
       "roleId": 2,
-    }, {
-      "id": 20,
+    }, {  
       "firstName": "Marlo",
       "lastName": "Rachuig",
       "email": "mrachuig9@so-net.ne.jp",
