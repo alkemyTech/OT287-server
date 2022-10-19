5871f9fe4d2cc47b98b4aa3c91d4c22fa043d8e2
Corxario <federicocapucci@hotmail.com>
2022-10-19 15:26:13 -0300

    Adding AWS SS urls to news seeder

--- database/seeders/20221017215034-create-news.js
+++ database/seeders/20221017215034-create-news.js
@@ -50,15 +50,6 @@ module.exports = {
       },
       {
         name: 'Taller de habilidades para los mas pequeños',
-        image: 'https://somosmasalkemy.s3.amazonaws.com/news/20221015152856_img5.png',
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo',
-        categoryId: '1',
-        type: 'news',
-        createdAt: new Date,
-        updatedAt: new Date
-      },
-      {
-        name: 'Talleres de arte para niños con carencias',
         image: 'https://somosmasalkemy.s3.amazonaws.com/news/20221015152856_img6.png',
         content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo',
         categoryId: '1',
@@ -67,7 +58,7 @@ module.exports = {
         updatedAt: new Date
       },
       {
-        name: 'Sponsoring de becas, vos podes formar parte',
+        name: 'Talleres de arte para niños con carencias',
         image: 'https://somosmasalkemy.s3.amazonaws.com/news/20221015152856_img7.png',
         content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo',
         categoryId: '1',
@@ -76,8 +67,8 @@ module.exports = {
         updatedAt: new Date
       },
       {
-        name: 'Nuevo logo!',
-        image: 'https://somosmasalkemy.s3.amazonaws.com/news/20221015152856_img1.png',
+        name: 'Sponsoring de becas, vos podes formar parte',
+        image: 'https://somosmasalkemy.s3.amazonaws.com/news/20221015152856_img8.png',
         content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo',
         categoryId: '1',
         type: 'news',
